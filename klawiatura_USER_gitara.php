00780ee997ef9b3e4dc2470f8e8d97195cbd5bdf
jakublenart1998 <59830025+jakublenart1998@users.noreply.github.com>
2020-01-19 13:49:23 +0100

    Add files via upload

---
+++ klawiatura_USER_gitara.php
@@ -0,0 +1,382 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="Jakub Lenart, Dominik Koziak, Jakub Lazar, Adrianna Kurzawa">
+  <title>Projekt zaliczeniowy</title>
+  <!-- Bootstrap CSS -->
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <link href="css/style.css" rel="stylesheet">
+  <link href="css/efekty.css" rel="stylesheet">
+  
+  <!-- Fonts -->
+  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href='https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+  <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+  <!-- Styles template -->
+  <link href="css/oriestroinator.min.css" rel="stylesheet">
+  <!-- silnik szczek i tabs -->
+  <script type="text/javascript" src="js/silnik_szczek.js"></script>
+  <script type="text/javascript" src="js/tabs.js"></script>
+    
+</head>
+  
+  
+  <!-- Navigation -->
+ <nav class="navbar navbar-expand-lg navbar-light fixed-top " id="mainNav">
+    <div class="container"> 
+        
+      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        Menu
+        <i class="fas fa-bars"></i>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="index_USER.php">Strona Główna</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="logowanie_USER.php">Logowanie</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="instrumenty_USER.php">Instrumenty</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="kontakt_USER.html">Kontakt</a>
+          </li>
+            <li class="nav-item">
+            <a class="nav-link" href="index.php">Wyloguj</a>
+          </li>
+        </ul>
+            
+      </div>
+    </div>
+  </nav>
+  <!-- Page Header -->
+  <header class="masthead" style="background-image: url('img/tape.jpg')">
+    <div class="overlay"></div>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 col-md-10 mx-auto">
+          <div class="site-heading">
+            <h1>ZAGRAJ</h1>
+            <span class="subheading"></span>
+              
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
+  <!-- Main Content -->
+      <hr>
+  <div class="container ">
+    <div class="row">    
+                            
+        
+        
+                    
+        
+        
+                         <div  class="col-sm">
+                        <a href="#" ><img  src="img/Ukulele-C-Q.png" alt="zdj" class="sepia card-img-top" ></a>
+                                <div class="card-body">
+                                    
+                                     
+                                </div>
+                        </div>
+        
+                        <div  class="col-sm">
+                        <a href="#" ><img  src="img/Ukulele-D-W.png" alt="zdj" class="sepia card-img-top" ></a>
+                                <div class="card-body">
+                                    
+                                     
+                                </div>
+                            </div>
+                        <div class="col-sm">
+                        <a href="#" ><img  src="img/Ukulele-E-E.png" alt="zdj" class="sepia card-img-top" ></a>
+                                <div class="card-body">
+                                    
+                                     
+                                </div>
+                    </div>
+                    
+        
+                    
+                        <div class="col-sm">
+                        <a href="#" ><img src="img/Ukulele-F-R.png" alt="zdj" class="sepia card-img-top" ></a>
+                                <div class="card-body">
+                                    
+                                     
+                                </div>
+                    </div>
+    
+    
+                        
+                        <div class="col-sm">
+                        <a href="#" ><img src="img/Ukulele-G-T.png" alt="zdj" class="sepia card-img-top" ></a>
+                                <div class="card-body">
+                                    
+                                     
+                                </div>
+                        </div>
+
+
+                        <div class="col-sm">
+                        <a href="#" ><img src="img/Ukulele-A-Y.png" alt="zdj" class="sepia card-img-top" ></a>
+                                <div class="card-body">
+                                    
+                                     
+                                </div>
+                    </div>
+        
+                    <div class="col-sm">
+                        <a href="#" ><img src="img/Ukulele-B-U.png" alt="zdj" class="sepia card-img-top" ></a>
+                                <div class="card-body">
+                                    
+                                     
+                                </div>
+                    </div>
+        
+                                
+                                
+                                        
+                    
+      </div>
+      <div class="row">
+          
+          <div  class="col-sm">
+                        <a href="#" ><img src="img/Ukulele-Cm-A.png" alt="zdj" class="sepia card-img-top" ></a>
+                                <div class="card-body">
+                                    
+                                     
+                                </div>
+                        </div>
+        
+                        <div  class="col-sm">
+                        <a href="#" ><img src="img/Ukulele-Dm-S.png" alt="zdj" class="sepia card-img-top" ></a>
+                                <div class="card-body">
+                                    
+                                     
+                                </div>
+                            </div>
+                        <div class="col-sm">
+                        <a href="#" ><img src="img/Ukulele-Em-D.png" alt="zdj" class="sepia card-img-top" ></a>
+                                <div class="card-body">
+                                    
+                                     
+                                </div>
+                    </div>
+                    
+        
+                    
+                        <div class="col-sm">
+                        <a href="#" ><img src="img/Ukulele-Fm-F.png" alt="zdj" class="sepia card-img-top" ></a>
+                                <div class="card-body">
+                                    
+                                     
+                                </div>
+                    </div>
+    
+    
+                        
+                        <div class="col-sm">
+                        <a href="#" ><img src="img/Ukulele-Gm-G.png" alt="zdj" class="sepia card-img-top" ></a>
+                                <div class="card-body">
+                                    
+                                     
+                                </div>
+                        </div>
+
+
+                        <div class="col-sm">
+                        <a href="#" ><img src="img/Ukulele-Am-H.png" alt="zdj" class="sepia card-img-top" ></a>
+                                <div class="card-body">
+                                    
+                                     
+                                </div>
+                    </div>
+        
+                    <div class="col-sm">
+                        <a href="#" ><img src="img/Ukulele-Bm-J.png" alt="zdj" class="sepia card-img-top" ></a>
+                                <div class="card-body">
+                                    
+                                     
+                                </div>
+                    </div>
+          
+          
+          
+          
+          
+      </div>
+      <div class="row">
+          
+          <div  class="col-sm">
+                        <a href="#" ><img src="img/Ukulele-C7-Z.png" alt="zdj" class="sepia card-img-top" ></a>
+                                <div class="card-body">
+                                    
+                                     
+                                </div>
+                        </div>
+        
+                        <div  class="col-sm">
+                        <a href="#" ><img src="img/Ukulele-D7-X.png" alt="zdj" class="sepia card-img-top" ></a>
+                                <div class="card-body">
+                                    
+                                     
+                                </div>
+                            </div>
+                        <div class="col-sm">
+                        <a href="#" ><img src="img/Ukulele-E7-C.png" alt="zdj" class="sepia card-img-top" ></a>
+                                <div class="card-body">
+                                    
+                                     
+                                </div>
+                    </div>
+                    
+        
+                    
+                        <div class="col-sm">
+                        <a href="#" ><img src="img/Ukulele-F7-V.png" alt="zdj" class="sepia card-img-top" ></a>
+                                <div class="card-body">
+                                    
+                                     
+                                </div>
+                    </div>
+    
+    
+                        
+                        <div class="col-sm">
+                        <a href="#" ><img src="img/Ukulele-G7-B.png" alt="zdj" class="sepia card-img-top" ></a>
+                                <div class="card-body">
+                                    
+                                     
+                                </div>
+                        </div>
+
+
+                        <div class="col-sm">
+                        <a href="#" ><img src="img/Ukulele-A7-N.png" alt="zdj" class="sepia card-img-top" ></a>
+                                <div class="card-body">
+                                    
+                                     
+                                </div>
+                    </div>
+        
+                    <div class="col-sm">
+                        <a href="#" ><img src="img/Ukulele-B7-M.png" alt="zdj" class="sepia card-img-top" ></a>
+                                <div class="card-body">
+                                    
+                                     
+                                </div>
+                    </div>
+          
+          
+          
+          
+      </div>
+      
+      
+      <div style="text-align: center;">
+        
+        <textarea id="in" rows="5" cols="40"></textarea><br>
+<button onclick="read();">Zagraj</button>
+<button onclick="stop();">Stop</button>
+<input type="checkbox" id="czyGra" checked> Graj
+  <div class="slidecontainer" style="margin-top: 50px;">
+  <input type="range" min="200" max="2000" id="zakres" style="width: 400px;"> </div>
+  <p>Prędkość odtwarzania w ms: <b id="demo"></b></p>
+  <script>
+ 
+ 
+var suwak = document.getElementById("zakres"); 
+var output = document.getElementById("demo");
+ 
+output.innerHTML = suwak.value;               
+ 
+suwak.oninput = function()                  
+{
+  output.innerHTML = this.value;
+}
+ 
+</script>
+   
+<div>
+  
+  <button onclick="generuj();">Generuj ukulele</button>
+  <button onclick="generuj2();">Generuj gitarę</button>
+</div>
+<!--tu się wyświetla tabulatura-->
+<div id='tab' style="font-family: monospace;"></div>
+        
+      </div>
+      <audio style="visibility: hidden;" id="z" src="szczek/szczek01.mp3" volume="0.1"></audio>
+<audio style="visibility: hidden;" id="s" src="szczek/szczek02.mp3" volume="0.1"></audio>
+<audio style="visibility: hidden;" id="x" src="szczek/szczek03.mp3" volume="0.1"></audio>
+<audio style="visibility: hidden;" id="d" src="szczek/szczek04.mp3" volume="0.1"></audio>
+<audio style="visibility: hidden;" id="c" src="szczek/szczek05.mp3" volume="0.1"></audio>
+<audio style="visibility: hidden;" id="v" src="szczek/szczek06.mp3" volume="0.1"></audio>
+<audio style="visibility: hidden;" id="g" src="szczek/szczek07.mp3" volume="0.1"></audio>
+<audio style="visibility: hidden;" id="b" src="szczek/szczek08.mp3" volume="0.1"></audio>
+<audio style="visibility: hidden;" id="h" src="szczek/szczek09.mp3" volume="0.1"></audio>
+<audio style="visibility: hidden;" id="n" src="szczek/szczek10.mp3" volume="0.1"></audio>
+<audio style="visibility: hidden;" id="j" src="szczek/szczek11.mp3" volume="0.1"></audio>
+<audio style="visibility: hidden;" id="m" src="szczek/szczek12.mp3" volume="0.1"></audio>
+<audio style="visibility: hidden;" id="," src="szczek/szczek13.mp3" volume="0.1"></audio>
+<audio style="visibility: hidden;" id="l" src="szczek/szczek14.mp3" volume="0.1"></audio>
+<audio style="visibility: hidden;" id="." src="szczek/szczek15.mp3" volume="0.1"></audio>
+<audio style="visibility: hidden;" id=";" src="szczek/szczek16.mp3" volume="0.1"></audio>
+<audio style="visibility: hidden;" id="/" src="szczek/szczek17.mp3" volume="0.1"></audio>
+    </div>
+      
+  <hr>
+  <!-- Footer -->
+  <footer>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 col-md-10 mx-auto">
+          <ul class="list-inline text-center">
+            <li class="list-inline-item">
+              <a href="#">
+                <span class="fa-stack fa-lg">
+                  <i class="fas fa-circle fa-stack-2x"></i>
+                  <i class="fab fa-twitter fa-stack-1x fa-inverse"></i>
+                </span>
+              </a>
+            </li>
+            <li class="list-inline-item">
+              <a href="#">
+                <span class="fa-stack fa-lg">
+                  <i class="fas fa-circle fa-stack-2x"></i>
+                  <i class="fab fa-facebook-f fa-stack-1x fa-inverse"></i>
+                </span>
+              </a>
+            </li>
+            <li class="list-inline-item">
+              <a href="#">
+                <span class="fa-stack fa-lg">
+                  <i class="fas fa-circle fa-stack-2x"></i>
+                  <i class="fab fa-instagram fa-stack-1x fa-inverse"></i>
+                </span>
+              </a>
+            </li>
+          </ul>
+            <br><hr>
+            
+            <p class="copyright text-muted">&copy;Orkiestroinator2019</p>   
+                       
+        </div>
+      </footer>
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <!-- scripts template -->
+  <script src="js/clean-blog.min.js"></script>
+        
+  
+</body>
+</html>
